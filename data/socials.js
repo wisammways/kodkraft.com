8a58b2702c2fc004ff1e809a25e510fddf774b9e
Wisam Mways <76596076+wisammways@users.noreply.github.com>
2025-10-03 11:23:34 +0300

    add youtube

--- data/socials.js
+++ data/socials.js
@@ -1,4 +1,5 @@
 export const socialLinks = [
   { icon: "uil-linkedin", unicode: "\\eaa2", color: "#e94d88", href: "https://www.linkedin.com/company/kodkraft-co" },
   { icon: "uil-instagram", unicode: "\\eb9c", color: "#d53581", href: "https://www.instagram.com/kodkraftagency" },
+  { icon: "uil-youtube", unicode: "\\eb8f", color: "#ff0000", href: "https://www.youtube.com/@KodKraft360" },
 ];
